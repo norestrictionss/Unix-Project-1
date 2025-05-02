68d641183e6da03fd71d17191c710ebf5fcb967b
Jedsam <fgokgoz02@hotmail.com>
2025-05-02 21:43:21 +0300

    Added required files for question 2 and added gitignore

---
+++ image_io.c
@@ -0,0 +1,62 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "image_io.h"
+
+unsigned char** read_pgm(const char* filename, int* width, int* height) {
+    FILE* fp = fopen(filename, "r");
+    if (!fp) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    char format[3];
+    fscanf(fp, "%s", format);
+    if (format[0] != 'P' || format[1] != '2') {
+        printf("Unsupported file format!\n");
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    fscanf(fp, "%d %d", width, height);
+    int max_value;
+    fscanf(fp, "%d", &max_value);
+
+    unsigned char** image = (unsigned char**)malloc((*height) * sizeof(unsigned char*));
+    for (int i = 0; i < *height; i++) {
+        image[i] = (unsigned char*)malloc((*width) * sizeof(unsigned char));
+        for (int j = 0; j < *width; j++) {
+            int pixel;
+            fscanf(fp, "%d", &pixel);
+            image[i][j] = (unsigned char)pixel;
+        }
+    }
+
+    fclose(fp);
+    return image;
+}
+
+void write_pgm(const char* filename, unsigned char** image, int width, int height) {
+    FILE* fp = fopen(filename, "w");
+    if (!fp) {
+        perror("Error writing file");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(fp, "P2\n%d %d\n255\n", width, height);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            fprintf(fp, "%d ", image[i][j]);
+        }
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
+}
+
+void free_image(unsigned char** image, int height) {
+    for (int i = 0; i < height; i++) {
+        free(image[i]);
+    }
+    free(image);
+}
