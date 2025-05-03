5e66da742e26cdcf1ad95240edbc71c5b392f2e1
Jedsam <fgokgoz02@hotmail.com>
2025-05-03 22:16:41 +0300

    base line?

--- q2.c
+++ q2.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "image_io.h"
+
+void startTimer(clock_t *time);
+void stopTimer(clock_t *time);
 
 int main() {
+    unsigned char **large_img, **med_img, **small_img;
+    int *width =  (int*)malloc(sizeof(int));
+    int *height =  (int*)malloc(sizeof(int));
 
-    printf("Hello World");
+    clock_t timer;
+    printf("Reading files\n");
+    startTimer(&timer);
 
+    large_img = read_pgm("input/large_1024x768.pgm", width, height);
+    med_img = read_pgm("input/medium_256x256.pgm", width, height);
+    small_img = read_pgm("input/small_10x10.pgm", width, height);
+
+    printf("Done reading files\n");
+    stopTimer(&timer);
+
+
+
+    free(width);
+    free(height);
     return 0;
+}
+
+void startTimer(clock_t *time) {
+    *time = clock();
+}
+void stopTimer(clock_t *time) {
+    printf("Time elapsed : %f s", (*time * 1.0) / CLOCKS_PER_SEC);
 }
